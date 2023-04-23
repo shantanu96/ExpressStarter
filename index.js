dc6a7d9698ad224c5806992f73cbba8d35cb79bb
Shantanu <shantanubhosale96@gmail.com>
2023-04-23 11:20:13 +0530

    Added UI pages that can display and add posts

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 
 import postRoutes from './src/routes/post.routes';
+import indexRoutes from './src/routes/index.routes';
 
 // Initialize Express app
 const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('trust proxy', true);
 
 // Set up body-parser middleware
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/', indexRoutes);
 
 // Connect to MongoDB database
 mongoose.connect('mongodb://localhost/blogweb', { useNewUrlParser: true, useUnifiedTopology: true })
